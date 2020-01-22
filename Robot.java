188da9e3486bec48e17aef4c4fe21e9debee4090
griffgies <59777735+griffgies@users.noreply.github.com>
2020-01-21 19:49:10 -0500

    Add files via upload

--- Robot.java
+++ Robot.java
@@ -1,5 +1,6 @@
 /*----------------------------------------------------------------------------*/
-/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.       
+                 */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
@@ -7,56 +8,168 @@
 
 package frc.robot;
 
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+
+import edu.wpi.first.cameraserver.CameraServer;
+import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
+import edu.wpi.first.wpilibj.GenericHID.Hand;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-
-
-import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkMaxLowLevel.MotorType;
-
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
- * This is a demo program showing the use of the RobotDrive class, specifically
- * it contains the code necessary to operate a robot with tank drive.
+ * The VM is configured to automatically run this class, and to call the
+ * functions corresponding to each mode, as described in the TimedRobot
+ * documentation. If you change the name of this class or the package after
+ * creating this project, you must also update the build.gradle file in the
+ * project.
  */
 public class Robot extends TimedRobot {
-  private DifferentialDrive m_myRobot;
-  private Joystick m_leftStick;
-  private Joystick m_rightStick;
-  private static final int leftDeviceID = 8; 
-  private static final int rightDeviceID = 3;
-  private static final int leftfDeviceID = 2; 
-  private static final int rightfDeviceID = 4;
-  private CANSparkMax m_leftMotor;
-  private CANSparkMax m_rightMotor;
-  private CANSparkMax m_leftfMotor;
-  private CANSparkMax m_rightfMotor;
+  private static final String kDefaultAuto = "Default";
+  private static final String kCustomAuto = "My Auto";
+  private String m_autoSelected;
+  private final SendableChooser<String> m_chooser = new SendableChooser<>();
+  WPI_TalonFX _frontLeftMotor = new WPI_TalonFX(3);
+  WPI_TalonFX _frontRightMotor = new WPI_TalonFX(4);
+  WPI_VictorSPX intake = new WPI_VictorSPX(10);
+
+  /* Construct drivetrain by providing master motor controllers */
+	DifferentialDrive _drive = new DifferentialDrive(_frontLeftMotor, _frontRightMotor);
+
+  /* Joystick for control */
+Joystick _joy = new Joystick(0);
+CameraServer server;
+Compressor c = new Compressor(0);
+
+
+    boolean enabled = c.enabled();
+    boolean pressureSwitch = c.getPressureSwitchValue();
+    double current = c.getCompressorCurrent();
+    DoubleSolenoid exampleDouble = new DoubleSolenoid(0, 1);
+
+  /**
+   * This function is run when the robot is first started up and should be
+   * used for any initialization code. 
+   */
   @Override
   public void robotInit() {
-    m_leftMotor = new CANSparkMax(leftDeviceID, MotorType.kBrushless);
-    m_rightMotor = new CANSparkMax(rightDeviceID, MotorType.kBrushless);
-    m_leftfMotor = new CANSparkMax(leftfDeviceID, MotorType.kBrushless);
-    m_rightfMotor = new CANSparkMax(rightfDeviceID, MotorType.kBrushless);
+    m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
+    m_chooser.addOption("My Auto", kCustomAuto);
+    SmartDashboard.putData("Auto choices", m_chooser);
+    _frontLeftMotor.configFactoryDefault();
+    _frontRightMotor.configFactoryDefault();
+    intake.configFactoryDefault();
+    _frontLeftMotor.setInverted(true); // <<<<<< Adjust this until robot drives forward when stick is forward
+    _frontRightMotor.setInverted(true);
+    intake.setInverted(false);
+    server = CameraServer.getInstance();
+    server.startAutomaticCapture();
 
-    m_leftMotor.restoreFactoryDefaults();
-    m_rightMotor.restoreFactoryDefaults();
-    m_leftfMotor.restoreFactoryDefaults();
-    m_rightfMotor.restoreFactoryDefaults();
-
-    m_myRobot = new DifferentialDrive(m_leftMotor, m_rightMotor);
-
-    m_leftfMotor.follow(m_leftMotor);
-    m_rightfMotor.follow(m_rightMotor);
-
-    m_leftStick = new Joystick(0);
-    //m_rightStick = new Joystick(1);
-  
+    
   }
 
+  /**
+   * This function is called every robot packet, no matter the mode. Use
+   * this for items like diagnostics that you want ran during disabled,
+   * autonomous, teleoperated and test.
+   *
+   * <p>This runs after the mode specific periodic functions, but before
+   * LiveWindow and SmartDashboard integrated updating.
+   */
+  @Override
+  public void robotPeriodic() {
+  }
+
+  /**
+   * This autonomous (along with the chooser code above) shows how to select
+   * between different autonomous modes using the dashboard. The sendable
+   * chooser code works with the Java SmartDashboard. If you prefer the
+   * LabVIEW Dashboard, remove all of the chooser code and uncomment the
+   * getString line to get the auto name from the text box below the Gyro
+   *
+   * <p>You can add additional auto modes by adding additional comparisons to
+   * the switch structure below with additional strings. If using the
+   * SendableChooser make sure to add them to the chooser code above as well.
+   */
+  @Override
+  public void autonomousInit() {
+    m_autoSelected = m_chooser.getSelected();
+    // m_autoSelected = SmartDashboard.getString("Auto Selector", kDefaultAuto);
+    System.out.println("Auto selected: " + m_autoSelected);
+  }
+
+  /**
+   * This function is called periodically during autonomous.
+   */
+  @Override
+  public void autonomousPeriodic() {
+    switch (m_autoSelected) {
+      case kCustomAuto:
+        // Put custom auto code here
+        break;
+      case kDefaultAuto:
+      default:
+        // Put default auto code here
+        break;
+    }
+  }
+
+  /**
+   * This function is called periodically during operator control.
+   */
   @Override
   public void teleopPeriodic() {
-    //this this broken. IT ONLY GOES BACK AND FORTH
-    m_myRobot.tankDrive(-m_leftStick.getY(), -m_leftStick.getY());
+
+    /* Gamepad processing */
+		double forward = -1.0 * _joy.getY(Hand.kLeft);	// Sign this so forward is positive
+    double turn = +1.0 * _joy.getX(Hand.kLeft);       // Sign this so right is positive
+    
+        
+        /* Deadband - within 10% joystick, make it zero */
+		if (Math.abs(forward) < 0.10) {
+			forward = 0;
+		}
+		if (Math.abs(turn) < 0.10) {
+			turn = 0;
+    }
+
+    if(_joy.getRawButton(1)){
+      exampleDouble.set(Value.kForward);
+    }
+  
+    if(_joy.getRawButton(2)){
+      exampleDouble.set(Value.kReverse);
+    }
+    intake.set(-_joy.getRawAxis(3)/2);        
+		/**
+		 * Print the joystick values to sign them, comment
+		 * out this line after checking the joystick directions. 
+		 */
+        System.out.println("JoyY:" + forward + "  turn:" + turn );
+        
+		/**
+		 * Drive the robot, 
+		 */
+    _drive.arcadeDrive(forward, turn);
+    SmartDashboard.putBoolean("Compressor", enabled);
+    SmartDashboard.putBoolean("Compressor Press", pressureSwitch);
+    SmartDashboard.putNumber("Intake Speed", -_joy.getRawAxis(3)/2);
+    SmartDashboard.putNumber("FWD Speed", forward);
+    SmartDashboard.putNumber("TURN Speed", turn);
+    SmartDashboard.updateValues();
+
+  }
+
+  /**
+   * This function is called periodically during test mode.
+   */
+  @Override
+  public void testPeriodic() {
   }
 }
