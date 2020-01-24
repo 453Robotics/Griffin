e3bee7bab81a4d53d29ddace7478225627544042
griffgies <59777735+griffgies@users.noreply.github.com>
2020-01-23 19:49:03 -0500

    Add files via upload

---
+++ Conveyor.java
@@ -0,0 +1,44 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.util.Color;
+import com.revrobotics.ColorSensorV3;
+import com.revrobotics.ColorMatch;
+import com.revrobotics.ColorMatchResult;
+
+
+
+
+
+public class Conveyor extends SubsystemBase {
+  WPI_VictorSPX intake = new WPI_VictorSPX(10);
+  Joystick _joy = new Joystick(0);
+  public Conveyor() {
+    intake.configFactoryDefault();
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+    if(_joy.getRawButton(1)){
+      
+    }
+  }
+}
