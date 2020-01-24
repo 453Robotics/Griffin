1ff3a3fa4f426b04df09b6c4af5a9e4af5ba1972
griffgies <59777735+griffgies@users.noreply.github.com>
2020-01-23 19:36:55 -0500

    Add files via upload

--- Robot.java
+++ Robot.java
@@ -1,6 +1,5 @@
 /*----------------------------------------------------------------------------*/
-/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.       
-                 */
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
@@ -8,168 +7,144 @@
 
 package frc.robot;
 
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
-import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
-
-import edu.wpi.first.cameraserver.CameraServer;
-import edu.wpi.first.wpilibj.Compressor;
-import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.Joystick;
+//import edu.wpi.first.wpilibj.smartdashboard.*;
+import edu.wpi.first.wpilibj.Controller;
+import edu.wpi.first.wpilibj.I2C;
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj.GenericHID.Hand;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.util.Color;
 
-/**
- * The VM is configured to automatically run this class, and to call the
- * functions corresponding to each mode, as described in the TimedRobot
- * documentation. If you change the name of this class or the package after
- * creating this project, you must also update the build.gradle file in the
- * project.
- */
+import com.revrobotics.ColorSensorV3;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.ColorMatch;
+import com.revrobotics.ColorMatchResult;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
 public class Robot extends TimedRobot {
-  private static final String kDefaultAuto = "Default";
-  private static final String kCustomAuto = "My Auto";
-  private String m_autoSelected;
-  private final SendableChooser<String> m_chooser = new SendableChooser<>();
-  WPI_TalonFX _frontLeftMotor = new WPI_TalonFX(3);
-  WPI_TalonFX _frontRightMotor = new WPI_TalonFX(4);
-  WPI_VictorSPX intake = new WPI_VictorSPX(10);
-
-  /* Construct drivetrain by providing master motor controllers */
-	DifferentialDrive _drive = new DifferentialDrive(_frontLeftMotor, _frontRightMotor);
-
-  /* Joystick for control */
-Joystick _joy = new Joystick(0);
-CameraServer server;
-Compressor c = new Compressor(0);
-
-
-    boolean enabled = c.enabled();
-    boolean pressureSwitch = c.getPressureSwitchValue();
-    double current = c.getCompressorCurrent();
-    DoubleSolenoid exampleDouble = new DoubleSolenoid(0, 1);
+  private final I2C.Port i2cPort = I2C.Port.kOnboard;
+  
+  private final ColorSensorV3 m_colorSensor = new ColorSensorV3(i2cPort);
+  private final ColorMatch m_colorMatcher = new ColorMatch();
 
   /**
-   * This function is run when the robot is first started up and should be
-   * used for any initialization code. 
+   * Note: Any example colors should be calibrated as the user needs, these
+   * are here as a basic example.
    */
-  @Override
-  public void robotInit() {
-    m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
-    m_chooser.addOption("My Auto", kCustomAuto);
-    SmartDashboard.putData("Auto choices", m_chooser);
-    _frontLeftMotor.configFactoryDefault();
-    _frontRightMotor.configFactoryDefault();
-    intake.configFactoryDefault();
-    _frontLeftMotor.setInverted(true); // <<<<<< Adjust this until robot drives forward when stick is forward
-    _frontRightMotor.setInverted(true);
-    intake.setInverted(false);
-    server = CameraServer.getInstance();
-    server.startAutomaticCapture();
+  private final Color kBlueTarget = ColorMatch.makeColor(0.143, 0.427, 0.429);
+  private final Color kGreenTarget = ColorMatch.makeColor(0.197, 0.561, 0.240);
+  private final Color kRedTarget = ColorMatch.makeColor(0.561, 0.232, 0.114);
+  private final Color kYellowTarget = ColorMatch.makeColor(0.361, 0.524, 0.113);
+  private DifferentialDrive m_myRobot;
+  //Create a joystick
+  private Joystick m_leftStick;
+  private Joystick m_rightStick;
+  
+  private static final int leftDeviceID = 8; 
+  private static final int rightDeviceID = 3;
+  private static final int leftFDeviceID = 2; 
+  private static final int rightFDeviceID = 4;
 
-    
-  }
+  private CANSparkMax m_leftMotor;
+  private CANSparkMax m_rightMotor;
+  private CANSparkMax m_leftFMotor;
+  private CANSparkMax m_rightFMotor;
 
-  /**
-   * This function is called every robot packet, no matter the mode. Use
-   * this for items like diagnostics that you want ran during disabled,
-   * autonomous, teleoperated and test.
-   *
-   * <p>This runs after the mode specific periodic functions, but before
-   * LiveWindow and SmartDashboard integrated updating.
-   */
+
   @Override
   public void robotPeriodic() {
-  }
+    /**
+     * The method GetColor() returns a normalized color value from the sensor and can be
+     * useful if outputting the color to an RGB LED or similar. To
+     * read the raw color, use GetRawColor().
+     * 
+     * The color sensor works best when within a few inches from an object in
+     * well lit conditions (the built in LED is a big help here!). The farther
+     * an object is the more light from the surroundings will bleed into the 
+     * measurements and make it difficult to accurately determine its color.
+     */
+    Color detectedColor = m_colorSensor.getColor();
 
-  /**
-   * This autonomous (along with the chooser code above) shows how to select
-   * between different autonomous modes using the dashboard. The sendable
-   * chooser code works with the Java SmartDashboard. If you prefer the
-   * LabVIEW Dashboard, remove all of the chooser code and uncomment the
-   * getString line to get the auto name from the text box below the Gyro
-   *
-   * <p>You can add additional auto modes by adding additional comparisons to
-   * the switch structure below with additional strings. If using the
-   * SendableChooser make sure to add them to the chooser code above as well.
-   */
-  @Override
-  public void autonomousInit() {
-    m_autoSelected = m_chooser.getSelected();
-    // m_autoSelected = SmartDashboard.getString("Auto Selector", kDefaultAuto);
-    System.out.println("Auto selected: " + m_autoSelected);
-  }
+    /**
+     * Run the color match algorithm on our detected color
+     */
+    String colorString;
+    ColorMatchResult match = m_colorMatcher.matchClosestColor(detectedColor);
+    
 
-  /**
-   * This function is called periodically during autonomous.
-   */
-  @Override
-  public void autonomousPeriodic() {
-    switch (m_autoSelected) {
-      case kCustomAuto:
-        // Put custom auto code here
-        break;
-      case kDefaultAuto:
-      default:
-        // Put default auto code here
-        break;
+    if (match.color == kBlueTarget) {
+      colorString = "Blue";
+    } else if (match.color == kRedTarget) {
+      colorString = "Red";
+    } else if (match.color == kGreenTarget) {
+      colorString = "Green";
+    } else if (match.color == kYellowTarget) {
+      colorString = "Yellow";
+    } else {
+      colorString = "Unknown";
     }
+
+    /**
+     * Open Smart Dashboard or Shuffleboard to see the color detected by the 
+     * sensor.
+     */
+    SmartDashboard.putNumber("Red", detectedColor.red);
+    SmartDashboard.putNumber("Green", detectedColor.green);
+    SmartDashboard.putNumber("Blue", detectedColor.blue);
+    SmartDashboard.putNumber("Confidence", match.confidence);
+    SmartDashboard.putString("Detected Color", colorString);
   }
 
+  @Override
+  public void robotInit() {
   /**
-   * This function is called periodically during operator control.
+   * SPARK MAX controllers are intialized over CAN by constructing a CANSparkMax object
+   * 
+   * The CAN ID, which can be configured using the SPARK MAX Client, is passed as the
+   * first parameter
+   * 
+   * The motor type is passed as the second parameter. Motor type can either be:
+   *  com.revrobotics.CANSparkMaxLowLevel.MotorType.kBrushless
+   *  com.revrobotics.CANSparkMaxLowLevel.MotorType.kBrushed
+   * 
+   * The example below initializes four brushless motors with CAN IDs 1 and 2. Change
+   * these parameters to match your setup
    */
+    m_colorMatcher.addColorMatch(kBlueTarget);
+    m_colorMatcher.addColorMatch(kGreenTarget);
+    m_colorMatcher.addColorMatch(kRedTarget);
+    m_colorMatcher.addColorMatch(kYellowTarget);  
+    m_leftMotor = new CANSparkMax(leftDeviceID, MotorType.kBrushless);
+    m_rightMotor = new CANSparkMax(rightDeviceID, MotorType.kBrushless);
+    m_leftFMotor = new CANSparkMax(leftFDeviceID, MotorType.kBrushless);
+    m_rightFMotor = new CANSparkMax(rightFDeviceID, MotorType.kBrushless);
+
+    /**
+     * The RestoreFactoryDefaults method can be used to reset the configuration parameters
+     * in the SPARK MAX to their factory default state. If no argument is passed, these
+     * parameters will not persist between power cycles
+     */
+
+    //just to make sure there normal
+    m_leftMotor.restoreFactoryDefaults();
+    m_rightMotor.restoreFactoryDefaults();
+    m_leftFMotor.restoreFactoryDefaults();
+    m_rightFMotor.restoreFactoryDefaults();
+
+    //creating a DifferentailDrive and giving it the 2 motors
+    m_leftFMotor.follow(m_leftMotor);
+    m_rightFMotor.follow(m_rightMotor);
+    m_myRobot = new DifferentialDrive(m_leftMotor, m_rightMotor);
+
+    m_leftStick = new Joystick(0);
+  }
+
   @Override
   public void teleopPeriodic() {
-
-    /* Gamepad processing */
-		double forward = -1.0 * _joy.getY(Hand.kLeft);	// Sign this so forward is positive
-    double turn = +1.0 * _joy.getX(Hand.kLeft);       // Sign this so right is positive
-    
-        
-        /* Deadband - within 10% joystick, make it zero */
-		if (Math.abs(forward) < 0.10) {
-			forward = 0;
-		}
-		if (Math.abs(turn) < 0.10) {
-			turn = 0;
-    }
-
-    if(_joy.getRawButton(1)){
-      exampleDouble.set(Value.kForward);
-    }
-  
-    if(_joy.getRawButton(2)){
-      exampleDouble.set(Value.kReverse);
-    }
-    intake.set(-_joy.getRawAxis(3)/2);        
-		/**
-		 * Print the joystick values to sign them, comment
-		 * out this line after checking the joystick directions. 
-		 */
-        System.out.println("JoyY:" + forward + "  turn:" + turn );
-        
-		/**
-		 * Drive the robot, 
-		 */
-    _drive.arcadeDrive(forward, turn);
-    SmartDashboard.putBoolean("Compressor", enabled);
-    SmartDashboard.putBoolean("Compressor Press", pressureSwitch);
-    SmartDashboard.putNumber("Intake Speed", -_joy.getRawAxis(3)/2);
-    SmartDashboard.putNumber("FWD Speed", forward);
-    SmartDashboard.putNumber("TURN Speed", turn);
-    SmartDashboard.updateValues();
-
-  }
-
-  /**
-   * This function is called periodically during test mode.
-   */
-  @Override
-  public void testPeriodic() {
+    //m_myRobot.tankDrive(m_leftStick.getY(), m_rightStick.getY());
+    m_myRobot.arcadeDrive(-m_leftStick.getY(Hand.kLeft), m_leftStick.getX(Hand.kLeft));
+    //SmartDashboard.putNumber("Left Drive Encoder", drivetrain.getLeftEncoder());
   }
 }
