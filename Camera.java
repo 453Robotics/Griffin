b8670852a27032e6bd2a717a9bca61e689a9e6fd
griffgies <59777735+griffgies@users.noreply.github.com>
2020-02-01 14:26:20 -0500

    Add files via upload

--- Camera.java
+++ Camera.java
@@ -5,11 +5,14 @@ import edu.wpi.first.cameraserver.CameraServer;
 public class Camera
 {
     CameraServer server;
+    CameraServer server2;
 
     public void init()
     {
         server = CameraServer.getInstance();
+        server2 = CameraServer.getInstance();
         server.startAutomaticCapture();
+        server2.startAutomaticCapture();
     }
 }
 
